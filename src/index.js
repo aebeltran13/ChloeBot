09e851ddc06d19f93e5b26d936881e1770c9bde6
Alfonso Beltran <aebeltran13@gmail.com>
2023-12-06 02:10:50 -0700

    Reformatted code

--- src/index.js
+++ src/index.js
@@ -1,31 +1,31 @@
-const {Client, IntentsBitField} = require('discord.js');
-require('dotenv').config();
+const { Client, IntentsBitField } = require("discord.js");
+require("dotenv").config();
 
 //Responses
-const {handleGreeting} = require('./responses/greetings');
-const {handleGoodbye} = require('./responses/goodbye');
+const { handleGreeting } = require("./responses/greetings");
+const { handleGoodbye } = require("./responses/goodbye");
 
 const client = new Client({
-    intents: [
-        IntentsBitField.Flags.Guilds,
-        IntentsBitField.Flags.GuildMembers,
-        IntentsBitField.Flags.GuildMessages,
-        IntentsBitField.Flags.MessageContent
-    ]
+  intents: [
+    IntentsBitField.Flags.Guilds,
+    IntentsBitField.Flags.GuildMembers,
+    IntentsBitField.Flags.GuildMessages,
+    IntentsBitField.Flags.MessageContent,
+  ],
 });
 
-client.once('ready', (c) => {
-    console.log(`${c.user.tag} has awoken... 👧🏼`);
-})
+client.once("ready", (c) => {
+  console.log(`${c.user.tag} has awoken... 👧🏼`);
+});
 
-client.on('messageCreate', (message) =>{
-    // Check if message is by human
-    if(message.author.bot){
-        return;
-    }
+client.on("messageCreate", (message) => {
+  // Check if message is by human
+  if (message.author.bot) {
+    return;
+  }
 
-    handleGreeting(message);
-    handleGoodbye(message);
-})
+  handleGreeting(message);
+  handleGoodbye(message);
+});
 
 client.login(process.env.DISCORD_BOT_TOKEN);
